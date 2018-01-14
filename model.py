7d1b905d01337fce55f83941783f56137ead57d3
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-01-14 16:43:09 -0500

    got 10 correctly classified '2's
    wrote sanity-check to make sure what we've done so far is correct

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import sys
 import os
 
 from tensorflow.examples.tutorials.mnist import input_data
+from scipy.misc import imsave
 
 import tensorflow as tf
 
@@ -156,6 +157,11 @@ def main(_):
         correct_prediction = tf.cast(correct_prediction, tf.float32)
     accuracy = tf.reduce_mean(correct_prediction)
 
+    with tf.name_scope('correct_twos'):
+        correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1))
+        is_two = tf.equal(tf.argmax(y_, 1), 2)
+    correct_twos = tf.logical_and(correct_prediction, is_two)
+
     # using with block means tf.Session() automatically destroyed when exit
     with tf.Session() as sess:
 
@@ -192,7 +198,46 @@ def main(_):
             y_: mnist.test.labels,
             keep_prob: 1}
         ))
-        # do adversarial examples
+
+        # get examples of correctly classified 2:
+
+        # correct_twos_eval[i] is True iff the model correctly predicts the
+        # ith image in MNIST test set as a '2'
+        correct_twos_eval = correct_twos.eval(feed_dict =
+            {x: mnist.test.images,
+            y_: mnist.test.labels,
+            keep_prob: 1}
+        )
+        # collect the indices
+        indices = []
+        for i in range(len(correct_twos_eval)):
+            if correct_twos_eval[i]:
+                indices.append(i)
+
+        # pseudo random shuffle of indices (to allow for replicability)
+        # remove the seed argument to have different shufflings
+        indices = tf.random_shuffle(indices, seed = 0)
+
+        # get the first ten
+        first_ten = indices[:10]
+
+        # sanity-check: make sure first_ten is as expected, and that we've
+        # actually saved images of '2'
+
+        # with seed = 0 argument in random_shuffle, should get
+        # [6041 2133 7271 3933 4418  738 7977 3274 9309 4905]
+        # note: it's important you only do sess.run(first_ten) once, otherwise
+        # you'll get a different array
+        eval_first_ten = sess.run(first_ten)
+        print(str(eval_first_ten))
+
+        # save images to disk to sanity-check that what we've done so far is
+        # correct, i.e. we actually have 10 images of '2'
+        for ind in eval_first_ten:
+            imsave('images/' + str(ind) + '.jpg', \
+                        sess.run(tf.reshape(mnist.test.images[ind], [28, 28])))
+
+        # comment out sanity-check before doing adversarial examples
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
